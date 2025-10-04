a199985fd38a1f631d6de9aee153e0693c4b3388
Palash Prakash <palashoffice2002@gmail.com>
2025-10-04 11:57:05 +0530

    Add files via upload

---
+++ FBS Assignment 0/Q9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int base,height,area;
+    printf("Enter the base of the Trianle: ");
+    scanf("%d",&base);
+    printf("Enter the height of the Triangle: ");
+    scanf("%d",&height);
+    area = 0.5*base*height;
+    printf("The area of the Triangle: %d",area);
+
+}
